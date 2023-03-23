a2152e4b413b5937d4c773b885ded93ef465c1ae
steven pedraza <as.pedraza@mensajerosurbanos.com>
2023-03-22 23:23:26 -0500

    commit donde cree la rama que se me olvido

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
 return (
 
     <h1 className="font-black text-4xl text-center md:w-1/2 mx-auto">
-        Seguimiento Pacientes {" "} con Github {' '}
+        Seguimiento Pacientes {" "} con Github2 {' '}
          <span className="text-indigo-600">Veterinaria</span> 
     </h1>
     
